7557f7590360d899302bc19710815fe055a70906
eromose-le <erons24@yahoo.com>
2022-04-13 16:24:30 +0100

    ffilter by Delivery or Pickup

--- components/home/HeaderTabs.js
+++ components/home/HeaderTabs.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function HeaderTabs(props) {
   return (
-    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+    <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
       <HeaderButton
         text="Delivery"
         btnColor="black"
@@ -25,18 +25,18 @@ export default function HeaderTabs(props) {
 const HeaderButton = (props) => (
   <TouchableOpacity
     style={{
-      backgroundColor: props.activeTab === props.text ? "black" : "white",
+      backgroundColor: props.activeTab === props.text ? 'black' : 'white',
       paddingVertical: 6,
       paddingHorizontal: 16,
-      borderRadius: 30,
+      borderRadius: 30
     }}
     onPress={() => props.setActiveTab(props.text)}
   >
     <Text
       style={{
-        color: props.activeTab === props.text ? "white" : "black",
+        color: props.activeTab === props.text ? 'white' : 'black',
         fontSize: 15,
-        fontWeight: "900",
+        fontWeight: '900'
       }}
     >
       {props.text}
