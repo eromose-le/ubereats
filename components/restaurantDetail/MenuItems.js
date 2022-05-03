66d4772975a0a7b53c4a4ab84b4b824da78c968c
eromose-le <erons24@yahoo.com>
2022-05-03 22:14:57 +0100

    set up react-redux and dispatched menuItems to store

--- components/restaurantDetail/MenuItems.js
+++ components/restaurantDetail/MenuItems.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 import React from 'react';
 import Divider from '../Divider';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import { useDispatch } from 'react-redux';
 
 const foods = [
   {
@@ -56,7 +57,19 @@ const styles = StyleSheet.create({
   }
 });
 
-export default MenuItems = () => {
+export default MenuItems = ({ restaurantName }) => {
+  const dispatch = useDispatch();
+  const selectItem = (item, checkboxValue) => {
+    dispatch({
+      type: 'ADD_TO_CART',
+      payload: {
+        ...item,
+        restaurantName: restaurantName,
+        checkboxValue: checkboxValue
+      }
+    });
+  };
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       {foods.map((food, index) => (
@@ -68,6 +81,7 @@ export default MenuItems = () => {
                 borderRadius: 0
               }}
               fillColor="green"
+              onPress={(checkboxValue) => selectItem(food, checkboxValue)}
             />
             <FoodInfo food={food} />
             <FoodImage food={food} />
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -5,13 +5,13 @@ import { About } from '../components/restaurantDetail/About';
 import MenuItems from '../components/restaurantDetail/MenuItems';
 import ViewCart from '../components/restaurantDetail/ViewCart';
 
-const RestaurantDetail = ({ route }) => {
+const RestaurantDetail = ({ route, navigation }) => {
   return (
     <View style={{ flex: 1 }}>
       <About route={route} />
       <Divider height={1.8} marginVertical={20} backgroundColor="#aaa" />
-      <MenuItems />
-      <ViewCart />
+      <MenuItems restaurantName={route.params.name} />
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
     </View>
   );
 };
